de97da71beece917d36d05e67acc808f51e4bf1b
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-05 19:48:00 -0700

    Restore from localstorage on reload

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
   // useful when saving the description in local storage?
 
 
-  setInterval(updatetime, 1000);
+  
 
 
   $('.saveBtn').click(function () {
@@ -32,5 +32,14 @@ $(document).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
   // TODO: Add code to display the current date in the header of the page.
 });
